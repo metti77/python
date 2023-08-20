a63a02a97b9f4e65aadc18d97670d3255c26702c
metti <mehdiasghari615@gmail.com>
2023-08-20 20:14:07 +0330

    string1

--- new.py
+++ new.py
@@ -2,3 +2,22 @@
 print(type(520))
 print(type('mamad'))
 print(type(52.3))
+print(22//5)  # rounded to nearest bedone ashar
+print(34%3)   # baghimande show mikonad
+print(7**2)   # tavaan
+x=int(9.3)
+y=str('6')
+z=float(3)
+print(x,y,z)
+o='reza'
+print(o[0:2])
+k='lorem\n ipsum\b dolor sit\t amet, \consectetur\ adip'
+print(k)
+name='jim'
+lastName='carry'
+age=45
+city='texas'
+job='developer'
+He=f'Im {name} {lastName} {age} from {city} as {job}'    #format string and concatenate
+print(He)
+
