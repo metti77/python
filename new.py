380dbe9863f1a84f8661451e77eb478531f9c130
metti <mehdiasghari615@gmail.com>
2023-08-28 20:16:14 +0330

    intro

--- new.py
+++ new.py
@@ -1,23 +1,9 @@
-# print('hello')
-print(type(520))
-print(type('mamad'))
-print(type(52.3))
-print(22//5)  # rounded to nearest bedone ashar
-print(34%3)   # baghimande show mikonad
-print(7**2)   # tavaan
-x=int(9.3)
-y=str('6')
-z=float(3)
-print(x,y,z)
-o='reza'
-print(o[0:2])
-k='lorem\n ipsum\b dolor sit\t amet, \consectetur\ adip'
-print(k)
-name='jim'
-lastName='carry'
-age=45
-city='texas'
-job='developer'
-He=f'Im {name} {lastName} {age} from {city} as {job}'    #format string and concatenate
-print(He)
-
+txt='she comes from italy'
+#print(txt.upper())
+# print(txt.count('o'))
+print(txt.title())
+print(bool(52))
+print(bool(None))
+x=8
+y=6
+print(x>=y)
