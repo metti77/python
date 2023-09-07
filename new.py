b37e32a3dabb04db5c3cdb243376b4b69afd1c3f
metti <mehdiasghari615@gmail.com>
2023-09-07 18:56:07 +0330

    dics

--- new.py
+++ new.py
@@ -1,5 +1,27 @@
-vart1={'iphone','sam','huawei','xiaomi'}
-vart2={'vintage','spigen','coco'}
-for x in vart1:
-#vart1.discard('sam')
-    print(x)
+dik= {
+    "sec1" :{
+    "name":"ali",
+    "family":"rad",
+    "age":"23",
+    "city":"tehran"
+},
+    "sec2":{
+    "car":"benz",
+    "color":"blue",
+    "model":"2008"
+    },
+    "sec3":{
+        "house":"town",
+        "street":"1st",
+        "no":"10th"
+    }
+}
+
+#z  = dik.keys()
+#z=dik.items()
+#dik["name"]="mamal"       #change 
+#dik.pop("age")              #pop
+#for x in dik.values() :   #for
+#dik2=dik.copy()
+print(dik["sec2"]["car"])
+
