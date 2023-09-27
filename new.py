f8bd88b66bb5f97527de4600defc06f0b1949f25
metti <mehdiasghari615@gmail.com>
2023-09-27 17:27:11 +0330

    dik

--- new.py
+++ new.py
@@ -1,9 +1,18 @@
-newset={'mouse','keyboard','monitor','speaker'}
-oldset={'fridge','glass','plate'}
-newset.update(oldset)
-newset.pop()
-#newset.remove('soin')
-#newset.add('power')
-print(
-    newset
-)
+firstdict= {
+    'model':'202',
+    'name':'rog',
+    'price':'$ 1899',
+    
+}
+a=firstdict.items()
+#a=firstdict.get('price')
+print(a)
+firstdict['color']='blue'
+print(a)
+if 'name' in firstdict :
+    print('ok correct metti %')
+    for x in firstdict.values():
+     print(x)
+newdict=firstdict.copy()     
+b=newdict.items()
+print(b)
