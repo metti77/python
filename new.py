ea3c9aaf538dba79b4e623b7ed5b01dc923612bc
metti <mehdiasghari615@gmail.com>
2023-09-27 18:57:42 +0330

    dikn

--- new.py
+++ new.py
@@ -1,18 +1,17 @@
-firstdict= {
-    'model':'202',
-    'name':'rog',
-    'price':'$ 1899',
-    
+products= {
+    'dik1':{
+        'model':'202',
+        'name':'rog',
+        'price':'$ 1899',
+    },
+    'dik2': {
+        'model':'301',
+        'name':'legion',
+        'price':'$ 1599',   
+    },
+    'dik3': {
+        'model':'501',
+        'name':'macbook',
+        'price':'$ 2199',
+    },
 }
-a=firstdict.items()
-#a=firstdict.get('price')
-print(a)
-firstdict['color']='blue'
-print(a)
-if 'name' in firstdict :
-    print('ok correct metti %')
-    for x in firstdict.values():
-     print(x)
-newdict=firstdict.copy()     
-b=newdict.items()
-print(b)
